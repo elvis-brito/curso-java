1aa5343f657f252e9eb988cd46735e75549344e9
Elvis Brito <elvisbritodossantos61@gmail.com>
2022-08-08 18:50:57 -0300

    Estudo java

---
+++ Exercicio1/entities/Product.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double TotalValueInStock() {
+		return quantity * price;
+	}
+	public void AddProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	public void RemoveProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	public String toString() {
+		return 	name
+			+ ", $ "
+			+ price
+			+ ", "
+			+ quantity
+			+ " units, Total: $ "
+			+ String.format("%.2f", TotalValueInStock());
+	}
+}
